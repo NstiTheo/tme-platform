42260b904211e86de8f41cf01d629f2b353cbc70
Nilo Theodoro <niloantonio09@gmail.com>
2026-05-23 18:42:03 -0300

    feat(auto-sync): atualizar camadas MVC
    Arquivos sincronizados automaticamente:
    - app/controllers/CertificateController.php
    - app/controllers/ProfileController.php
    - app/controllers/RankingController.php

---
+++ app/controllers/RankingController.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASE_PATH') || exit('Acesso direto nao permitido.');
+
+class RankingController extends Controller
+{
+    public function index(): void
+    {
+        $gamification = new Gamification();
+        $courseId = (int) ($_GET['course_id'] ?? 0);
+
+        $this->view('gamification/ranking', [
+            'title' => 'Ranking',
+            'ranking' => $gamification->ranking($courseId ?: null),
+            'courses' => $gamification->coursesForRanking(),
+            'selectedCourseId' => $courseId,
+        ]);
+    }
+}
